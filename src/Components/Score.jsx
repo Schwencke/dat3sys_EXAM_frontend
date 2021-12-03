7c70b8420d9088880f5700d5b5259fa0294f5e0e
PeterDaniel3D <75070076+PeterDaniel3D@users.noreply.github.com>
2021-12-03 09:25:53 +0100

    Change start score

--- src/Components/Score.jsx
+++ src/Components/Score.jsx
@@ -1,22 +1,23 @@
 import { useState, useEffect } from "react"
 
-const Score = ({win, firstGame, toggle}) => {
+const Score = ({ win, firstGame, toggle }) => {
 
-    const [score, setScore] = useState(0)
+  const [score, setScore] = useState(1)
 
-    const incrementScore = () => {
-      setScore(score +1)
+  const incrementScore = () => {
+    setScore(score + 1)
+  }
+
+  useEffect(() => {
+    if (!firstGame) {
+      if (win === true) return incrementScore()
+      if (win === false) return setScore("You lost!: " + score)
     }
+  }, [win, toggle])
 
-    useEffect(() => {
-      if (!firstGame){
-     if (win === true) return incrementScore()
-     if (win === false) return setScore ("You lost!: " + score)
-    }}, [win,toggle])
-
-    return (
-        <p>The Score is: {score}</p>
-    )
+  return (
+    <p>The Score is: {score}</p>
+  )
 }
 
 export default Score
